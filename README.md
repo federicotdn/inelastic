17e468bc5698aa6c46ea3456e2c243fddac4e5bf
federicotdn <federicotedin.ar@gmail.com>
2018-08-13 10:35:52 -0300

    Add verbose mode

--- README.md
+++ README.md
@@ -4,17 +4,23 @@ Print an Elasticsearch inverted index as a table (CSV) or JSON object.
 ## Usage
 
 ```bash
-usage: inelastic.py [-h] -i <name> [-e <host>] [-p <port>] [-d <type>] -f <field>
-                    [-o <format>]
+usage: inelastic.py [-h] -i <name> [-e <host>] [-p <port>] [-d <type>] -f <field> [-v] [-o <format>]
 
 optional arguments:
   -h, --help            show this help message and exit
   -i <name>, --index <name>
+                        Index name.
   -e <host>, --host <host>
+                        Elasticsearch host address.
   -p <port>, --port <port>
+                        Elasticsearch host port.
   -d <type>, --doctype <type>
+                        Document type.
   -f <field>, --field <field>
+                        Document field.
+  -v, --verbose         Enable verbose mode.
   -o <format>, --output <format>
+                        Output format. Use 'null' to omit output.
 ```
 
 ## Examples
@@ -23,3 +29,8 @@ optional arguments:
 $ ./inelastic.py -f content -i tweets -o csv > tweets.csv
 $ ./inelastic.py -f description -i products -o json > products.json
 ```
+
+## Todo
+- Add better examples
+- Add more term statistics
+- Handle same term appearing multiple times in the same doc
--- inelastic.py
+++ inelastic.py
@@ -5,17 +5,23 @@ import sys
 import json
 from collections import defaultdict
 from elasticsearch import Elasticsearch, ElasticsearchException
+from tqdm import tqdm
 
-SEARCH_SIZE = 50
+SEARCH_SIZE = 200
 SCROLL_TIME = '1m'
 CSV_SEP = ','
 CSV_EOL = '\n'
+
+
+def vprint(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
 
 
 class InvertedIndex:
     def __init__(self):
         self._max_term_freq = 0
         self._term_dict = defaultdict(list)
+        self._sorted_terms = []
         self._dirty = True
 
     def add_terms(self, doc_id, terms):
@@ -26,53 +32,61 @@ class InvertedIndex:
 
         self._dirty = True
 
-    def _sort_ids(self):
+    def _sort(self):
         if not self._dirty:
             return
 
         for ids in self._term_dict.values():
             ids.sort()
 
+        self._sorted_terms = sorted(self._term_dict.items(),
+                                    key=lambda i: i[0])
+
         self._dirty = False
 
     def write_csv(self, fp):
-        self._sort_ids()
+        self._sort()
 
         fields = ['term', 'freq']
         fields.extend('d{}'.format(i) for i in range(self._max_term_freq))
         fp.write('{}{}'.format(CSV_SEP.join(fields), CSV_EOL))
 
-        sorted_terms = sorted(self._term_dict.items(),
-                              key=lambda i: i[0])
-
-        for term, ids in sorted_terms:
+        for term, ids in self._sorted_terms:
             fp.write('{}{}'.format(term, CSV_SEP))
             fp.write('{}{}'.format(len(ids), CSV_SEP))
             fp.write('{}{}'.format(CSV_SEP.join(ids), CSV_EOL))
 
     def write_json(self, fp):
-        self._sort_ids()
-
-        sorted_terms = sorted(self._term_dict.items(),
-                              key=lambda i: i[0])
+        self._sort()
 
         obj = {
             'terms': [{
                 'term': term,
                 'freq': len(ids),
                 'ids': ids
-            } for term, ids in sorted_terms]
+            } for term, ids in self._sorted_terms]
         }
 
         json.dump(obj, fp, indent=4, ensure_ascii=False)
 
 
-def get_inverted_index(es, index, doc_type, field):
+def get_inverted_index(es, index, doc_type, field, verbose):
+    if verbose:
+        doc_count = es.count(index=index)['count']
+        vprint('Index: {}'.format(index))
+        vprint('Document type: {}'.format(doc_type))
+        vprint('Document field: {}'.format(field))
+        vprint('Document count: {}'.format(doc_count))
+
     search = es.search(index=index, scroll=SCROLL_TIME, _source=False, body={
         'size': SEARCH_SIZE
     })
 
+    errors = 0
     inv_index = InvertedIndex()
+    if verbose:
+        vprint('Reading term vectors...')
+        pbar = tqdm(total=doc_count, file=sys.stderr)
 
     while search['hits']['hits']:
         hits = search['hits']['hits']
@@ -83,11 +97,22 @@ def get_inverted_index(es, index, doc_type, field):
 
         for result in resp['docs']:
             doc_id = result['_id']
-            terms = result['term_vectors'][field]['terms'].keys()
-            inv_index.add_terms(doc_id, terms)
+            if 'term_vectors' in result:
+                terms = result['term_vectors'][field]['terms'].keys()
+                inv_index.add_terms(doc_id, terms)
+            else:
+                errors += 1
+
+        if verbose:
+            pbar.update(len(ids))
 
         scroll_id = search['_scroll_id']
         search = es.scroll(scroll_id, scroll=SCROLL_TIME)
+
+    if verbose:
+        pbar.close()
+
+    vprint('Done ({} mterm vectors errors).'.format(errors))
 
     es.clear_scroll(scroll_id)
     return inv_index
@@ -95,29 +120,47 @@ def get_inverted_index(es, index, doc_type, field):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--index', metavar='<name>', required=True)
-    parser.add_argument('-e', '--host', metavar='<host>', default='localhost')
-    parser.add_argument('-p', '--port', metavar='<port>', default=9200)
-    parser.add_argument('-d', '--doctype', metavar='<type>', default='_doc')
-    parser.add_argument('-f', '--field', metavar='<field>', required=True)
+    parser.add_argument('-i', '--index', metavar='<name>', required=True,
+                        help='Index name.')
+    parser.add_argument('-e', '--host', metavar='<host>', default='localhost',
+                        help='Elasticsearch host address.')
+    parser.add_argument('-p', '--port', metavar='<port>', default=9200,
+                        help='Elasticsearch host port.')
+    parser.add_argument('-d', '--doctype', metavar='<type>', default='_doc',
+                        help='Document type.')
+    parser.add_argument('-f', '--field', metavar='<field>', required=True,
+                        help='Document field.')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Enable verbose mode.')
     parser.add_argument('-o', '--output', metavar='<format>', default='json',
-                        choices=['json', 'csv'])
+                        choices=['json', 'csv', 'null'],
+                        help='Output format. Use \'null\' to omit output.')
     args = parser.parse_args()
+
+    if not args.verbose:
+        global vprint
+        vprint = lambda *args, **kwargs: None  # noqa: E731
 
     es = Elasticsearch(args.host, port=args.port)
 
+    vprint('Starting inelastic script...')
+
     try:
         inv_index = get_inverted_index(es, args.index, args.doctype,
-                                       args.field)
+                                       args.field, args.verbose)
     except ElasticsearchException as e:
         print('An Elasticsearch error occurred:', file=sys.stderr)
         print(e, file=sys.stderr)
         exit(1)
 
+    vprint('Writing inverted index in {} format...'.format(args.output))
+
     if args.output == 'csv':
         inv_index.write_csv(sys.stdout)
     elif args.output == 'json':
         inv_index.write_json(sys.stdout)
+
+    vprint('All done.')
 
 
 if __name__ == '__main__':
